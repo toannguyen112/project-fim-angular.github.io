4542f4c9195f8e512d9b0aa9f4c8c1abd7bee6d3
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2020-03-11 21:57:25 +0700

    cap nhat

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,16 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { AdminManageComponent } from './admin-manage/admin-manage.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminLayoutComponent, AdminManageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class AdminModule { }
