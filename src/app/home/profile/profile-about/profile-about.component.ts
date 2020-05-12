b2e8e334244d6daf48ad48f264f5cac5eafed701
nguyencongtoana1nhvt@gmail.com <toannguyen112>
2020-05-12 11:06:26 +0700

    cap nhat

--- src/app/home/profile/profile-about/profile-about.component.ts
+++ src/app/home/profile/profile-about/profile-about.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProfileAboutComponent implements OnInit {
   @Input("thongTinTaiKhoan") thongTinTaiKhoan  ; 
-  public capNhatAvarta : string ="https://scontent.fsgn6-2.fna.fbcdn.net/v/t1.0-0/p160x160/49308610_106238597147928_3522069505675100160_n.jpg?_nc_cat=102&_nc_sid=7aed08&_nc_ohc=QzWJMvZ1ZwoAX-RKvUr&_nc_ht=scontent.fsgn6-2.fna&_nc_tp=6&oh=906e63d9464a59297130a8a77c4be99b&oe=5EA5010F";
+  public capNhatAvarta : string ="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXT-MvunybNC8rfKP3k9axIiv4G8HRZIskEF5RZIRilJCvHnP-&usqp=CAU";
   constructor() { }
   public currentInput ;
   ngOnInit() {
